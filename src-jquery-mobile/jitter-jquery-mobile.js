77a17088890fe604c8f8abafc6366ee3ae10a16f
Florent Legendre <florent.legendre@bouvet.no>
2011-09-01 08:20:19 +0200

    bugfixes for local run

--- src-jquery-mobile/jitter-jquery-mobile.js
+++ src-jquery-mobile/jitter-jquery-mobile.js
@@ -9,7 +9,7 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
             queryUrl: 'http://api.twitter.com/1/statuses/public_timeline.json?callback=?',
             template: htmlTemplateAsString
         });
-    });
+    }, "html");
 }
 else {
     alert('The File APIs are not fully supported in this browser.');
